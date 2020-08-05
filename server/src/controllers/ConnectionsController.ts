0fd45c45fffec3acd358ebf76955b00d930ab525
Daniel Felizardo <daniel.david772@gmail.com>
2020-08-05 04:59:36 -0300

    Create server application - 2 dia

---
+++ server/src/controllers/ConnectionsController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+
+import db from '../database/connection';
+
+class ConnectionsController {
+  async index (request: Request, response: Response): Promise<Response> {
+    const totalConnections = await db('connections').count('* as total');
+
+    const { total } = totalConnections[0];
+
+    return response.send();
+  }
+
+  async create (request: Request, response: Response): Promise<Response> {
+    const { user_id } = request.body;
+
+    await db('connections').insert({
+      user_id,
+    });
+
+    return response.status(201).send();
+  }
+}
+
+export default ConnectionsController;
